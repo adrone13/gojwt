32c436f86700cd30d7b9d3dc6e34e8b82befb020
aleksandr.s <alex@Aleksandrs-MacBook-Pro.local>
2024-03-26 20:36:02 +0200

    comment cleanup

--- claims.go
+++ claims.go
@@ -3,10 +3,10 @@ package jwt
 // Claims for JWT token (RFC 7519)
 // https://www.iana.org/assignments/jwt/jwt.xhtml
 type Claims struct {
-	Issuer     string   `json:"iss"` // Issuer (e.g. Auth service)
-	Expiration int64    `json:"exp"` // Expiration timestamp
-	Audience   string   `json:"aud"` // Audience - service for which JWT is intended
-	Subject    string   `json:"sub"` // Subject - user identity
+	Issuer     string   `json:"iss"`
+	Expiration int64    `json:"exp"`
+	Audience   string   `json:"aud"`
+	Subject    string   `json:"sub"`
 	Name       string   `json:"name"`
 	Roles      []string `json:"roles"`
 }
